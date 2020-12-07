fixed issue -> normalization made problems

GatedPixelCNN/layers.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class GatedMaskedConv2d(nn.Conv2d):
-    def __init__(self, mask_type, mask_orientation, color_channels=False, **kwargs):
+    def __init__(self, mask_type, mask_orientation, dependent_colors=False, **kwargs):
         """
         Masked 2D-Convolution
 
@@ -16,49 +16,56 @@ class GatedMaskedConv2d(nn.Conv2d):
         :param int pad: padding
         """
         super(GatedMaskedConv2d, self).__init__(**kwargs, bias=False)
+        ch_out, _, _, _ = self.weight.size()
         assert mask_type in ['A', 'B']
         assert mask_orientation in ['vertical', 'horizontal']
-        self.create_mask(mask_type, mask_orientation)
+        self.create_mask(mask_type, mask_orientation, dependent_colors)
 
-    def create_mask(self, mask_type, mask_orientation):
+    def create_mask(self, mask_type, mask_orientation, dependent_colors):
         ch_out, ch_in, height, width = self.weight.size()
-        mask = torch.ones(ch_out, ch_in, height, width, dtype=torch.uint8)
-        mask[:, :, height // 2 + 1:, :] = 0
+        self.register_buffer('mask', torch.zeros_like(self.weight, dtype=torch.uint8))
         if mask_orientation == 'horizontal':
-            mask[:, :, :, width // 2 + 1:] = 0
-            mask[:, :, :height // 2, :] = 0
-        if mask_type == 'A':
-            # First Convolution Only
-            # => Restricting connections to
-            #    already predicted neighboring channels in current pixel
-            mask[:, :, height // 2, width // 2] = 0
-            if mask_orientation == 'vertical':
-                mask[:, :, height // 2, :] = 0
-
-        assert ch_out % 3 == 0 and ch_in % 3 == 0
-        one_third_in, one_third_out = ch_out // 3, ch_in // 3
-        if mask_type == 'B':
-            self.mask[:one_third_out, :one_third_in, height // 2, width // 2] = 1
-            self.mask[one_third_out:2 * one_third_out, :2 * one_third_in, height // 2, width // 2] = 1
-            self.mask[2 * one_third_out:, :, height // 2, width // 2] = 1
+            self.mask[:, :, height // 2, :width // 2] = 1
+            if dependent_colors:
+                self.dependent_color_masking(self.weight.size(), mask_type, mask_orientation)
+            elif mask_type == 'B':
+                self.mask[:, :, height // 2, width // 2] = 1
         else:
-            self.mask[one_third_out:2 * one_third_out, :one_third_in, height // 2, width // 2] = 1
-            self.mask[2 * one_third_out:, :2 * one_third_in, height // 2, width // 2] = 1
+            self.mask[:, :, :height // 2, :] = 1
+            if dependent_colors:
+                self.dependent_color_masking(self.weight.size(), mask_type, mask_orientation)
+            else:
+                self.mask[:, :, height // 2] = 1
 
-        self.register_buffer('mask', mask)
-        # if class_cond:
-        #     self.class_cond = nn.Linear(1, ch_out)
+    def dependent_color_masking(self, shape, mask_type, mask_orientation):
+        ch_out, ch_in, height, width = shape
+        assert ch_out % 3 == 0 and ch_in % 3 == 0
+        one_third_in, one_third_out = ch_in // 3, ch_out // 3
+        if mask_orientation == 'horizontal':
+            if mask_type == 'B':
+                self.mask[:one_third_out, :one_third_in, height // 2, width // 2] = 1
+                self.mask[one_third_out:2 * one_third_out, :2 * one_third_in, height // 2, width // 2] = 1
+                self.mask[2 * one_third_out:, :, height // 2, width // 2] = 1
+            else:
+                self.mask[one_third_out:2 * one_third_out, :one_third_in, height // 2, width // 2] = 1
+                self.mask[2 * one_third_out:, :2 * one_third_in, height // 2, width // 2] = 1
+        else:
+            if mask_type == 'B':
+                self.mask[:one_third_out, :one_third_in, height // 2] = 1
+                self.mask[one_third_out:2 * one_third_out, :2 * one_third_in, height // 2] = 1
+                self.mask[2 * one_third_out:, :, height // 2] = 1
+            else:
+                self.mask[one_third_out:2 * one_third_out, :one_third_in, height // 2] = 1
+                self.mask[2 * one_third_out:, :2 * one_third_in, height // 2] = 1
 
-    def forward(self, x, y=None):
+    def forward(self, x):
         self.weight.data *= self.mask
         out = super(GatedMaskedConv2d, self).forward(x)
-        if y is not None:
-            out = out + self.class_cond(y).view(y.shape[0], -1, 1, 1)
         return out
 
 
 class MaskedConv2D(nn.Conv2d):
-    def __init__(self, mask_type, class_cond=None, **kwargs):
+    def __init__(self, mask_type, dependent_colors=False, num_classes=None, **kwargs):
         """
         Masked 2D-Convolution
 
@@ -72,24 +79,33 @@ class MaskedConv2D(nn.Conv2d):
         super(MaskedConv2D, self).__init__(**kwargs, bias=False)
         self.mask_type = mask_type
         ch_out, ch_in, height, width = self.weight.size()
-        mask = torch.ones(ch_out, ch_in, height, width, dtype=torch.uint8)
-        if mask_type == 'A':
-            # First Convolution Only
-            # => Restricting connections to
-            #    already predicted neighboring channels in current pixel
-            mask[:, :, height // 2, width // 2:] = 0
+        self.register_buffer('mask', torch.zeros_like(self.weight, dtype=torch.uint8))
+        self.mask[:, :, :height // 2] = 1
+        self.mask[:, :, height // 2, :width // 2] = 1
+        if dependent_colors:
+            self.dependent_color_masking(self.weight.size(), mask_type)
+        elif mask_type == 'B':
+            self.mask[:, :, height // 2, width // 2] = 1
+        if num_classes is not None:
+            self.cond_bias = nn.Linear(num_classes, ch_out)
+
+    def dependent_color_masking(self, weight_shape, mask_type):
+        ch_out, ch_in, height, width = weight_shape
+        assert ch_out % 3 == 0 and ch_in % 3 == 0
+        one_third_in, one_third_out = ch_in // 3, ch_out // 3
+        if mask_type == 'B':
+            self.mask[:one_third_out, :one_third_in, height // 2, width // 2] = 1
+            self.mask[one_third_out:2 * one_third_out, :2 * one_third_in, height // 2, width // 2] = 1
+            self.mask[2 * one_third_out:, :, height // 2, width // 2] = 1
         else:
-            mask[:, :, height // 2, width // 2 + 1:] = 0
-        mask[:, :, height // 2 + 1:] = 0
-        self.register_buffer('mask', mask)
-        if class_cond:
-            self.class_cond = nn.Linear(1, ch_out)
+            self.mask[one_third_out:2 * one_third_out, :one_third_in, height // 2, width // 2] = 1
+            self.mask[2 * one_third_out:, :2 * one_third_in, height // 2, width // 2] = 1
 
     def forward(self, x, y=None):
         self.weight.data *= self.mask
         out = super(MaskedConv2D, self).forward(x)
         if y is not None:
-            out = out + self.class_cond(y).view(y.shape[0], -1, 1, 1)
+            out = out + self.cond_bias(y).view(y.shape[0], -1, 1, 1)
         return out
 
 
@@ -113,8 +129,8 @@ class GatedActivation(nn.Module):
 
 
 class LayerNorm(nn.LayerNorm):
-    def __init__(self, dependent_colors=False, *args, **kwargs):
-        super(LayerNorm, self).__init__(*args, **kwargs)
+    def __init__(self, num_filters, dependent_colors=False, *args, **kwargs):
+        super(LayerNorm, self).__init__(num_filters, *args, **kwargs)
         self.dependent_colors = dependent_colors
 
     def forward(self, x):
@@ -130,10 +146,11 @@ class LayerNorm(nn.LayerNorm):
 
 class StackLayerNorm(nn.Module):
   def __init__(self, n_filters, dependent_colors=False):
-    super().__init__()
+    super(StackLayerNorm, self).__init__()
     self.h_layer_norm = LayerNorm(n_filters, dependent_colors)
     self.v_layer_norm = LayerNorm(n_filters, dependent_colors)
 
   def forward(self, vertical, horizontal):
     return self.v_layer_norm(vertical), self.h_layer_norm(horizontal)
+
 

GatedPixelCNN/model.py
@@ -8,41 +8,44 @@ from utils import DEVICE
 
 
 class GatedBlock(nn.Module):
-    def __init__(self, mask_type, in_channels, out_channels, kernel_size, stride=1):
+    def __init__(self, mask_type, in_channels, out_channels, kernel_size, stride=1, dependent_colors=False,
+                 num_classes=None):
         super(GatedBlock, self).__init__()
         assert mask_type in ['A', 'B']
-        padding = int((kernel_size - stride) / 2)
+        padding = int((kernel_size - stride) / 2) # stride = 1
         # vertical stack
         self.vertical = GatedMaskedConv2d(mask_type=mask_type, mask_orientation='vertical',
                                           in_channels=in_channels, out_channels=2 * out_channels,
-                                          kernel_size=kernel_size, padding=padding)
+                                          kernel_size=kernel_size, padding=padding, stride=stride,
+                                          dependent_colors=dependent_colors)
         self.v_to_h = nn.Conv2d(2 * out_channels, 2 * out_channels, kernel_size=1, bias=False)
         # horizontal stack
         self.horizontal = GatedMaskedConv2d(mask_type=mask_type, mask_orientation='horizontal',
                                             in_channels=in_channels, out_channels=2 * out_channels,
-                                            kernel_size=(1, kernel_size), padding=(0, padding))
+                                            kernel_size=(1, kernel_size), padding=(0, padding), stride=stride,
+                                            dependent_colors=dependent_colors)
         self.mask_type = mask_type
         self.res = nn.Conv2d(out_channels, out_channels, kernel_size=1, bias=False)
         if mask_type == 'B':
             self.skip = nn.Conv2d(out_channels, out_channels, kernel_size=1, bias=False)
+        if num_classes is not None:
+            self.cond_bias_v = nn.Linear(num_classes, 2 * out_channels)
+            self.cond_bias_h = nn.Linear(num_classes, 2 * out_channels)
         self.activation = GatedActivation()
         self.mask_type = mask_type
 
-    # def down_shift(self, x):
-    #     # crops top row
-    #     # remove last row
-    #     x = x[:, :, :-1, :]
-    #     pad = nn.ZeroPad2d((0, 0, 1, 0))
-    #     return pad(x)
-
-    def forward(self, x_vertical, x_horizontal):
+    def forward(self, x_vertical, x_horizontal, y=None):
         # vertical stack NxN
         x_vertical = self.vertical(x_vertical)
+        if y is not None:
+            x_vertical = x_vertical + self.cond_bias_v(y).view(y.shape[0], -1, 1, 1)
         x_vertical_stack = self.activation(x_vertical)
         # horizontal stack 1xN
         x_h_stack = self.horizontal(x_horizontal)
         # info from vertical to horizontal
         x_h_stack = x_h_stack + self.v_to_h(x_vertical)
+        if y is not None:
+            x_h_stack = x_h_stack + self.cond_bias_h(y).view(y.shape[0], -1, 1, 1)
         x_h_stack = self.activation(x_h_stack)
 
         x_res = self.res(x_h_stack)
@@ -52,31 +55,36 @@ class GatedBlock(nn.Module):
             x_res = x_res + x_horizontal
             skip_output = self.skip(x_h_stack)
         return x_vertical_stack, x_res, skip_output
-        # return x_vertical_stack, x_res
 
 
 class GatedPixelCNN(nn.Module):
-    def __init__(self, input_shape, num_colors, num_layers=8, num_filters=128, output_filters=32):
+    def __init__(self, input_shape, num_colors, num_layers=8, num_h_filters=128, num_o_filters=30,
+                 dependent_colors=False, num_classes=None):
         super(GatedPixelCNN, self).__init__()
+        kwargs = {'dependent_colors': dependent_colors, 'num_classes': num_classes}
         C, H, W = input_shape
         self.num_channels = C
         self.input_shape = input_shape
         self.num_colors = num_colors
 
-        self.input = GatedBlock(mask_type='A', in_channels=C, out_channels=num_filters, kernel_size=7)
+        self.input = GatedBlock(mask_type='A', in_channels=C, out_channels=num_h_filters, kernel_size=7, **kwargs)
         self.gated_blocks = nn.ModuleList()
         for _ in range(num_layers):
-            # StackLayerNorm(num_filters),
-            self.gated_blocks.extend([
-                                      GatedBlock('B', num_filters, num_filters, kernel_size=7)])
-        self.output = nn.Sequential(nn.ReLU(), MaskedConv2D(mask_type='B', in_channels=num_filters,
-                                                            out_channels=output_filters, kernel_size=1),
+            # if dependent_colors:
+            #     self.gated_blocks.append(StackLayerNorm(num_h_filters // 3, dependent_colors=dependent_colors))
+            # else:
+            #     self.gated_blocks.append(StackLayerNorm(num_h_filters, dependent_colors=dependent_colors))
+            self.gated_blocks.append(GatedBlock('B', in_channels=num_h_filters, out_channels=num_h_filters,
+                                                kernel_size=7, **kwargs))
+        self.output = nn.Sequential(nn.ReLU(), MaskedConv2D(mask_type='B', in_channels=num_h_filters,
+                                                            out_channels=num_o_filters, kernel_size=1,
+                                                            **kwargs),
                                     nn.ReLU(),
                                     MaskedConv2D(mask_type='B',
-                                                 in_channels=output_filters, out_channels=num_colors * C,
-                                                 kernel_size=1))
+                                                 in_channels=num_o_filters, out_channels=num_colors * C,
+                                                 kernel_size=1, **kwargs))
 
-    def forward(self, x):
+    def forward(self, x, y=None):
         batch_size = x.shape[0]
         # scale input
         out = x.float()
@@ -88,16 +96,20 @@ class GatedPixelCNN(nn.Module):
             if isinstance(gated_block, StackLayerNorm):
                 vertical, horizontal = gated_block(vertical, horizontal)
             else:
-                vertical, horizontal, skip = gated_block(vertical, horizontal)
+                vertical, horizontal, skip = gated_block(vertical, horizontal, y)
                 skip_sum += skip
         # horizontal output
-        out = self.output(skip_sum)
-        return out.view(batch_size, self.num_channels, self.num_colors, *self.input_shape[1:]).permute(0, 2, 1, 3, 4)
+        for layer in self.output:
+            if isinstance(layer, MaskedConv2D):
+                skip_sum = layer(skip_sum, y)
+            else:
+                skip_sum = layer(skip_sum)
+        return skip_sum.view(batch_size, self.num_channels, self.num_colors, *self.input_shape[1:]).permute(0, 2, 1, 3, 4)
 
-    def loss(self, x):
-        return F.cross_entropy(self(x), x.long())
+    def loss(self, x, y=None):
+        return F.cross_entropy(self(x, y), x.long())
 
-    def sample(self, n, visible=False):
+    def sample(self, n, y=None, visible=False):
         C, H, W = self.input_shape
         samples = torch.zeros(n, *self.input_shape).to(DEVICE)
         with torch.no_grad():
@@ -105,7 +117,7 @@ class GatedPixelCNN(nn.Module):
                 for row in range(H):
                     for col in range(W):
                         for channel in range(C):
-                            logits = self(samples)[:, :, channel, row, col]
+                            logits = self(samples, y)[:, :, channel, row, col]
                             probs = F.softmax(logits, dim=1)
                             samples[:, channel, row, col] = torch.multinomial(probs, 1).squeeze(-1)
                             if prog is not None:

utils/train.py
@@ -1,11 +1,12 @@
 import numpy as np
 import torch
+import torch.nn.functional as F
 from tqdm import tqdm
 
 from utils import DEVICE
 
 
-def train_epoch(model, data_loader, optimizer, use_targets=False, grad_clip=None, scheduler=None,
+def train_epoch(model, data_loader, optimizer, num_classes=False, grad_clip=None, scheduler=None,
                 visible=None, binarize=True):
     """
     Train model for 1 epoch and return dictionary with the average training metric values
@@ -23,9 +24,9 @@ def train_epoch(model, data_loader, optimizer, use_targets=False, grad_clip=None
     model.train(mode=True)
     batch_losses = []
     for batch_idx, batch in enumerate(data_loader):
-        x, y = process_data(batch, use_targets, binarize)
+        x, y = process_data(batch, num_classes, binarize)
         batch_size = x.shape[0]
-        if use_targets:
+        if num_classes:
             batch_loss = model.loss(x, y)
         else:
             batch_loss = model.loss(x)
@@ -45,7 +46,7 @@ def train_epoch(model, data_loader, optimizer, use_targets=False, grad_clip=None
     return np.mean(batch_losses)
 
 
-def evaluate(model, data_loader, use_targets=False, binarize=True):
+def evaluate(model, data_loader, num_classes=None, binarize=True):
     """
 
     :param model:
@@ -56,9 +57,9 @@ def evaluate(model, data_loader, use_targets=False, binarize=True):
     total_loss = 0
     with torch.no_grad():
         for batch in data_loader:
-            x, y = process_data(batch, use_targets, binarize)
+            x, y = process_data(batch, num_classes, binarize)
             batch_size = x.shape[0]
-            if use_targets:
+            if num_classes:
                 batch_loss = model.loss(x, y)
             else:
                 batch_loss = model.loss(x)
@@ -66,10 +67,13 @@ def evaluate(model, data_loader, use_targets=False, binarize=True):
     return total_loss.item() / len(data_loader)
 
 
-def process_data(batch, use_targets, binarize):
+def process_data(batch, num_classes, binarize):
+    if not isinstance(batch, list):
+        return batch.to(DEVICE), None
     x, y = batch
     if binarize:
         x = (x > 0.5).byte()
-    if use_targets:
+    if num_classes:
+        y = F.one_hot(y, num_classes).float()
         return x.to(DEVICE), y.to(DEVICE)
     return x.to(DEVICE), None